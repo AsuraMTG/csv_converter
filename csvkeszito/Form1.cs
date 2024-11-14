c86583c100023e8163e915c09db7ee7d9159f40f
Masznyik Marton <masmar575@logiker.hu>
2024-11-14 09:41:47 +0100

    2024.11.14

--- csvkeszito/Form1.cs
+++ csvkeszito/Form1.cs
@@ -27,6 +27,9 @@ namespace csvkeszito
         }
         public EtteremAdatok etteremFeltoltese = new EtteremAdatok();
         public List<EtteremAdatok> etterem = new List<EtteremAdatok>();
+
+        
+
         public string seged;
         public string seged2;
         public int intseged;
@@ -117,7 +120,76 @@ namespace csvkeszito
             folyam.Close();
             return a;
         }
+        public char[] kategoriak = { 'L', 'F', 'S', 'T', 'Z', 'D' };
+        public void Menu()
+        {
+            /*
+            int counter = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = i + 1; j < 5; j++)
+                {
+                    for (int k = j + 1; k < 6; k++)
+                    {
+                        
+                    }
+                }
+            }
+            label4.Text = counter.ToString();
+            */
 
+            intseged = 10000;
+            string[] legkisebbArKategoriakSzerint = new string[kategoriak.Length];
+            for (int i = 0; i < kategoriak.Length; i++)
+            {
+                for (int j = 0; j < etterem.Count; j++)
+                {
+                    if (etterem[j].kategoria[0] == kategoriak[i])
+                    {
+                        if (etterem[j].ar < intseged)
+                        {
+                            intseged = j;
+                        }
+                    }
+                }
+                legkisebbArKategoriakSzerint[i] = $"{etterem[intseged].kategoria}, {etterem[intseged].megnevezes}, {etterem[intseged].ar}";
+            }
+        }
+
+        /*
+        public void Menu()
+        {
+            label4.Text = "";
+            intseged = 0;
+            int counter = 0;
+            if (textBox1.Text.Length == 3)
+            {
+                if (kategoriak.Contains(textBox1.Text[counter]))
+                {
+                    for (int i = 0; i < textBox1.Text.Length; i++)
+                    {
+                        if (kategoriak.Contains(textBox1.Text[i]))
+                        {
+                            for (int j = 0; j < etterem.Count; j++)
+                            {
+                                if (etterem[j].kategoria[0] == textBox1.Text[i])
+                                {
+                                    if (etterem[j].ar > intseged)
+                                    {
+                                        intseged = j;
+                                    }
+                                }
+                            }
+                        }
+                        label4.Text += $"{etterem[intseged].kategoria};{etterem[intseged].megnevezes};{etterem[intseged].ar}\n";
+                    }
+                }
+                counter++;
+            }
+            else
+                label4.Text = "HIBAS BEMENET";
+        }
+        */
         /*
             try
             {
@@ -140,13 +212,23 @@ namespace csvkeszito
         {
             label1.Text = "";
             label2.Text = "";
+            label4.Text = "";
             Beolvaso("étterem.txt");
+            /*
             for (int i = 0; i < etterem.Count; i++)
             {
                 label1.Text += $"{etterem[i].kategoria};{etterem[i].megnevezes};{etterem[i].kcal};{etterem[i].szenhidrat};{etterem[i].ar}\n";
                 intseged += etterem[i].ar;
             }
             label2.Text = $"{intseged / etterem.Count}";
+            */
+
+            label3.Text = "3db -> L F S T Z D";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Menu();
         }
     }
 }
